c99ddf965a44b35da0614394d72bc4b58fa1d137
Tom van Neerijnen <tom@tomvn.com>
2015-09-10 08:45:45 +0100

    move to hooks

--- lib/decrypt_and_run.sh
+++ lib/decrypt_and_run.sh
@@ -3,19 +3,4 @@
 # fail fast on errors
 set -e
 
-PROC=$1
-
-KEY_PROVIDER=${KEY_PROVIDER:=dummy}
-
-# move the encrypted tar and key to the work directory
-shopt -s dotglob nullglob
-ENCR_APP=$(mktemp -d)
-mv ./* ${ENCR_APP}/
-
-PASSPHRASE=$(cat ${ENCR_APP}/encr_key | ${ENCR_APP}/lib/providers/${KEY_PROVIDER} decrypting)
-
-gpg --batch --no-tty --passphrase="${PASSPHRASE}" --no-use-agent -d ${ENCR_APP}/slug.tgz.gpg | tar -xzf - --strip 2 -C ./ ./app/
-PROC_CMD=$(cat /app/Procfile | grep ^$PROC | cut -f 2- -d ':')
-source .profile.d/*
-rm -fr ${ENCR_APP}
-exec $PROC_CMD
+source ./lib/run_hooks $(pwd)
