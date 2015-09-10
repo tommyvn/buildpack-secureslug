28a2ef48c7756c357b26c38814432469905981c2
Tom van Neerijnen <tom@tomvn.com>
2015-09-10 08:42:00 +0100

    simplify variable and add worker type to decrypt_and_run

--- lib/decrypt_and_run.sh
+++ lib/decrypt_and_run.sh
@@ -3,6 +3,8 @@
 # fail fast on errors
 set -e
 
+PROC=$1
+
 KEY_PROVIDER=${KEY_PROVIDER:=dummy}
 
 # move the encrypted tar and key to the work directory
@@ -13,7 +15,6 @@ mv ./* ${ENCR_APP}/
 PASSPHRASE=$(cat ${ENCR_APP}/encr_key | ${ENCR_APP}/lib/providers/${KEY_PROVIDER} decrypting)
 
 gpg --batch --no-tty --passphrase="${PASSPHRASE}" --no-use-agent -d ${ENCR_APP}/slug.tgz.gpg | tar -xzf - --strip 2 -C ./ ./app/
-PROC=${DYNO%.*}
 PROC_CMD=$(cat /app/Procfile | grep ^$PROC | cut -f 2- -d ':')
 source .profile.d/*
 rm -fr ${ENCR_APP}
